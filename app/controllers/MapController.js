05fe35123ca887a4c56e5816c0542368d299b292
yogesh-harbinger <yogesh.more@SCS-I02.harbinger.in>
2015-10-08 16:26:54 +0530

    displatyed record not found message

--- app/controllers/MapController.js
+++ app/controllers/MapController.js
@@ -18,7 +18,8 @@ sampleApp.controller('MapController', function($rootScope,$scope,sampleAppLib,$f
         UserService.query().$promise.then(function(userListResponse) {
             // Use of angularJS filter functionality to get specific record
             $scope.userData = $filter("filter")( userListResponse , {id:parseInt(userID)},true)[0];
-            if(typeof $scope.userData != undefined ){
+            $scope.recordFound = true;            
+            if(typeof $scope.userData != 'undefined'){
                 // As Address is auto-generated they are not valid so passing complete address may not give any latlong object so passing only country
                 $scope.addressString = $scope.userData.address.street_address+',<br>'+$scope.userData.address.city+',<br>'+$scope.userData.address.region+',<br>'+$scope.userData.address.country;
                 // Call getcoder to get latlong object for address
@@ -28,8 +29,8 @@ sampleApp.controller('MapController', function($rootScope,$scope,sampleAppLib,$f
                         if (status == google.maps.GeocoderStatus.OK) {
                             if (status != google.maps.GeocoderStatus.ZERO_RESULTS) {
                                 $scope.LatLongObj = results[0].geometry.location;
-                                
-                                // Now setting Marker position                            
+
+                                // Now setting Marker position
                                 mapObj.setCenter($scope.LatLongObj);
                                 var infowindow = new google.maps.InfoWindow({
                                     content: '<div><b>' + $scope.addressString + '</b></div>',
@@ -39,10 +40,10 @@ sampleApp.controller('MapController', function($rootScope,$scope,sampleAppLib,$f
                                     position: $scope.LatLongObj,
                                     title: 'Address',
                                     map: mapObj
-                                }); 
+                                });
                                 google.maps.event.addListener(marker, 'click', function() {
                                     infowindow.open(mapObj, marker);
-                                });                                
+                                });
                             } else {
                                 // No latitude longitude found
                                 console.log('Latitude and Longitude not found for address '+address);
@@ -50,8 +51,10 @@ sampleApp.controller('MapController', function($rootScope,$scope,sampleAppLib,$f
                         } else {
                             console.log('Geocode was successful with status '+ status);
                         }
-                    });            
+                    });
                 }
+            } else {
+                 $scope.recordFound = false;
             }
         });
     }
--- app/controllers/UserController.js
+++ app/controllers/UserController.js
@@ -1,15 +1,19 @@
 /**
- * User controller 
+ * User controller
 */
 
 sampleApp.controller('UserController', function($rootScope,$scope,sampleAppLib,$filter,UserService,$stateParams) {
     // Fetch data from resource
-    // In real application application must fetch data using GET method and for specific r 
+    // In real application application must fetch data using GET method and for specific r
     userID = $stateParams.id;
     if(userID){
         UserService.query().$promise.then(function(userListResponse) {
             // Use of angularJS filter functionality to get specific record
             $scope.userDataSet = $filter("filter")( userListResponse , {id:parseInt(userID)},true)[0];
+            $scope.recordFound = true;            
+            if(typeof $scope.userDataSet == 'undefined'){
+                $scope.recordFound = false;
+            }
         });
     }
 });
