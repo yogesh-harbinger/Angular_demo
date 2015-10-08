89378abe631ab0e1d7510f6054403e40a307d038
yogesh-harbinger <yogesh.more@SCS-I02.harbinger.in>
2015-10-08 13:52:00 +0530

    updated scope of userdataset from rootscope to scope as required changes are implemented in map controller

--- app/controllers/DashboardController.js
+++ app/controllers/DashboardController.js
@@ -5,7 +5,7 @@
 sampleApp.controller('DashboardController', function($rootScope,$scope,sampleAppLib,userList,$resource) {    
     // Fetch data from resource created to get all user's data
     userList.query().$promise.then(function(userListResponse) {
-        //Data set in rootscope to use in map controller
-        $rootScope.userDataSet = userListResponse;
+        //Data set contains all user's information
+        $scope.userDataSet = userListResponse;
     });
 });
