106a483f2d6ae19abc39e707cda5cb9914d9c8c5
yogesh-harbinger <yogesh.more@SCS-I02.harbinger.in>
2015-10-08 11:52:02 +0530

    Modfied controller to fetch user data from json file using resource and show marker by getting latitude longitude using geocoder api and also added merker to show tooltip with address

--- app/controllers/MapController.js
+++ app/controllers/MapController.js
@@ -1,25 +1,66 @@
 /**
- * Map controller 
+ * Map controller
 */
 
-sampleApp.controller('MapController', function($rootScope,$scope,sampleAppLib,$filter,userList,$stateParams) {    
+sampleApp.controller('MapController', function($rootScope,$scope,sampleAppLib,$filter,userList,$stateParams) {
     // Following method uses the json object set in rootscope. This can also be achieved by creating service and then calling service functions to read data from server instead of rootscope
     // In actual implementation application should fetch data from API
+    /*
     userID = $stateParams.id;
-    if(userID){        
-        $scope.userData = $filter("filter")( $rootScope.userDataSet , {id:parseInt(userID)},true)[0];    
-    }
-    
-    // Map Object initialization and call to view user's location
+    if(userID){
+        $scope.userData = $filter("filter")( $rootScope.userDataSet , {id:parseInt(userID)},true)[0];
+    } */
+
+    // Map Object initialization
     var mapObj;
+    $scope.LatLongObj = undefined;
     $scope.$on('mapInitialized', function(event, map) {
-        //Basic configuration        
+        //Basic configuration
         mapObj = map;
-        mapObj.setZoom(8); // Zoom level        
-        mapObj.setCenter(new google.maps.LatLng($scope.userData.address.latitude, $scope.userData.address.longitude));
-        marker = new google.maps.Marker({
-            position: new google.maps.LatLng($scope.userData.address.latitude, $scope.userData.address.longitude),
-            map: map
+        mapObj.setZoom(8); // Zoom level
+
+    });
+
+    userID = $stateParams.id;
+    if(userID){
+        userList.query().$promise.then(function(userListResponse) {
+            // Use of angularJS filter functionality to get specific record
+            $scope.userData = $filter("filter")( userListResponse , {id:parseInt(userID)},true)[0];
+            if(typeof $scope.userData != undefined ){
+                // As Address is auto-generated they are not valid so passing complete address may not give any latlong object so passing only country
+                $scope.addressString = $scope.userData.address.street_address+',<br>'+$scope.userData.address.city+',<br>'+$scope.userData.address.region+',<br>'+$scope.userData.address.country;
+                // Call getcoder to get latlong object for address
+                geocoder = new google.maps.Geocoder();
+                if (geocoder) {
+                    geocoder.geocode({'address': $scope.userData.address.country}, function(results, status) {
+                        if (status == google.maps.GeocoderStatus.OK) {
+                            if (status != google.maps.GeocoderStatus.ZERO_RESULTS) {
+                                $scope.LatLongObj = results[0].geometry.location;
+                                
+                                // Now setting Marker position                            
+                                mapObj.setCenter($scope.LatLongObj);
+                                var infowindow = new google.maps.InfoWindow({
+                                    content: '<div><b>' + $scope.addressString + '</b></div>',
+                                    size: new google.maps.Size(150, 50)
+                                });
+                                marker = new google.maps.Marker({
+                                    position: $scope.LatLongObj,
+                                    title: 'Address',
+                                    map: mapObj
+                                }); 
+                                google.maps.event.addListener(marker, 'click', function() {
+                                    infowindow.open(mapObj, marker);
+                                });                                
+                            } else {
+                                // No latitude longitude found
+                                console.log('Latitude and Longitude not found for address '+address);
+                            }
+                        } else {
+                            console.log('Geocode was successful with status '+ status);
+                        }
+                    });            
+                }
+            }
         });
-    });    
+    }
 });
